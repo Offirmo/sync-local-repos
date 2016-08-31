832787aeebdc058bc4b56fd09a36982182f22a55
Offirmo <offirmo.net@gmail.com>
2016-08-31 19:10:17 +0200

    +

---
+++ get_command_output.js
@@ -0,0 +1,102 @@
+// share code executing a command and returning its output
+
+import spawn from 'cross-spawn';
+//const spawn = require('child_process').spawn
+
+export default function get_command_output(
+	executable,
+	options
+) {
+	options = options || {};
+	options.params = options.params || [];
+	options.timeout = options.timeout || 3000;
+	options.env = process.env;
+	options.verbose = true
+	// options.merge_stderr
+
+	return new Promise((resolve, reject) => {
+		options.verbose && console.log(`Spawn : spawning ${executable}`, options.params.join(' ') || '' );
+		const spawn_instance = spawn(executable, options.params, options);
+		const radix = 'Spawn#' + spawn_instance.pid;
+
+		options.verbose && console.log(`${radix}: spawned ${executable} ${options.params.join(' ') || ''}${options.cwd ? ' @' + options.cwd : ''}`,  );
+
+		let stdout = ""
+		let stderr = ""
+
+		let is_finished = false
+		function finish(err = null) {
+			if (is_finished) return
+			is_finished = true
+
+			if (err) {
+				err.message = err.message + ' [err]' + stderr + ' [out]' + stdout
+				return reject(err)
+			}
+
+			if (stderr)
+				return reject(new Error('got stderr:' + stderr + ' [out]' + stdout))
+
+			resolve(stdout.trim())
+		}
+
+		spawn_instance.on('error', err => {
+			options.verbose && console.log(`${radix}: got err:`, err);
+			finish(err);
+		});
+		spawn_instance.on('close', (code, signal) => {
+			options.verbose && console.log(`${radix}: got event close with code "${code}" & signal "${signal}"`)
+			if (code !== 0)
+				finish(new Error(`Spawn: child process #${spawn_instance.pid} closed with code ${code}`));
+			else
+				finish()
+		});
+		spawn_instance.on('disconnect', () => {
+			options.verbose && console.log(`${radix}: got disconnect`)
+			finish()
+		});
+		spawn_instance.on('exit', (code, signal) => {
+			options.verbose && console.log(`${radix}: got event exit with code "${code}" & signal "${signal}"`)
+			if (code !== 0)
+				finish(new Error(`Spawn: child process #${spawn_instance.pid} exited with code ${code}`));
+			else
+				finish()
+		});
+
+		if (spawn_instance.stdin) {
+			spawn_instance.stdin.on('data', data => {
+				options.verbose && console.log(`${radix}: got stdin data event : "${data}"`);
+			});
+			spawn_instance.stdin.on('error', err => {
+				options.verbose && console.log(`${radix}: got stdin error event : "${err}"`);
+				finish(err)
+			});
+		}
+
+		if (spawn_instance.stdout) {
+			spawn_instance.stdout.on('data', data => {
+				options.verbose && console.log(`${radix}: got stdout data event : "${data}"`);
+				stdout += data
+			});
+			spawn_instance.stdout.on('error', err => {
+				options.verbose && console.log(`${radix}: got stdout error event : "${err}"`);
+				finish(err)
+			});
+		}
+
+
+		if (spawn_instance.stderr) {
+			spawn_instance.stderr.on('data', data => {
+				options.verbose && console.log(`${radix}: got stderr data event : "${data}"`);
+				if (options.merge_stderr)
+					stdout += data
+				else
+					stderr += data
+			});
+			spawn_instance.stderr.on('error', err => {
+				options.verbose && console.log(`${radix}: got stderr error event : "${err}"`);
+				finish(err)
+			});
+		}
+	});
+}
---
+++ index.js
@@ -0,0 +1,193 @@
+#!/bin/sh
+':' //# http://sambal.org/?p=1014 ; exec `dirname $0`/node_modules/.bin/babel-node "$0" "$@"
+'use strict';
+
+import get_command_output from './get_command_output';
+
+
+console.log('Hello world !')
+
+
+const path = require('path')
+const _ = require('@offirmo/cli-toolbox/lodash')
+const visual_tasks = require('@offirmo/cli-toolbox/stdout/visual_tasks')
+const fs = require('@offirmo/cli-toolbox/fs/extra')
+const json = require('@offirmo/cli-toolbox/fs/json')
+const prettify_json = require('@offirmo/cli-toolbox/string/prettify-json')
+const stylize_string = require('@offirmo/cli-toolbox/string/stylize')
+const log_symbols = require('@offirmo/cli-toolbox/string/log-symbols')
+const meow = require('@offirmo/cli-toolbox/framework/meow')
+const tildify = require('@offirmo/cli-toolbox/string/tildify')
+
+require('@offirmo/cli-toolbox/stdout/clear-cli')()
+
+let REPO_DIRS = []
+
+const cli = meow(`
+    Usage
+      $ ./index.js <path>
+
+    Options
+      --dry-run  don't touch anything
+
+    Examples
+      $ ./index.js .. --dry-run
+`)
+
+const repos_parent_dir = cli.input[0]
+const options = cli.flags
+const DEV_NPM_MODULES = []
+
+console.log('* path:', repos_parent_dir)
+console.log('* options:', options)
+
+console.log('* Gathering list of repos...')
+REPO_DIRS = fs.lsDirs(repos_parent_dir).map(repo_dir => path.join(repos_parent_dir, repo_dir))
+console.log('* Processing repos...')
+Promise.all(
+	REPO_DIRS
+		//.slice(0, 1)
+		.map(repo_dir => process_dir(repo_dir, options))
+)
+.then(res => {
+	console.log('Done.')
+	console.log('will exit in 3s...')
+	setTimeout(() => process.exit(0), 3000)
+})
+.catch((err) => {
+	console.error(stylize_string.red.bold(log_symbols.error + prettify_json(err)))
+	cli.showHelp(1)
+	process.exit(1)
+})
+
+
+
+function process_dir(dir, options) {
+	console.log('* processing repo ' + tildify(dir))
+
+	let is_git_repo = true
+	let is_npm_module = true
+
+	const preconditions = Promise.resolve(true)
+		.then(() => {
+			console.log(`  Checking if git repo: "${dir}"`)
+			return get_command_output(`test`, {
+				params: '-d .git'.split(' '),
+				cwd: dir
+			})
+				.catch(() => is_git_repo = false)
+		})
+		.then(() => {
+			console.log(`  Checking if npm module: "${dir}"`)
+			return get_command_output(`test`, {
+				params: '-f package.json'.split(' '),
+				cwd: dir
+			})
+				.catch(() => is_npm_module = false)
+		})
+
+		const actions = preconditions
+			.then(() => {
+				if (!is_git_repo)
+					return console.log(`  ${log_symbols.info} "${dir}" skipping git operations since not a git repo`)
+				if (options.dryRun)
+					return console.log(`  ${log_symbols.warning} "${dir}" skipping git operations due to dry run`)
+				return update_git_related(dir, options)
+			})
+			.then(() => {
+				if (!is_npm_module)
+					return console.log(`  ${log_symbols.info} "${dir}" skipping npm operations since not a npm module`)
+				if (options.dryRun)
+					return console.log(`  ${log_symbols.warning} "${dir}" skipping npm operations due to dry run`)
+				return update_npm_related(dir, options)
+			})
+
+		return actions
+}
+
+
+function update_git_related(repo_dir, options) {
+	console.log('  update_git_related', tildify(repo_dir), options)
+
+	let git_branch
+	let is_repo_dirty = false
+
+	const observations = Promise.resolve(true)
+		.then(() => {
+			console.log(`  Checking git branch of "${repo_dir}"`)
+			return get_command_output(`git`, {
+				params: 'rev-parse --abbrev-ref HEAD'.split(' '),
+				cwd: repo_dir
+			})
+			.then(data => git_branch = data)
+		})
+		.then(() => {
+			console.log(`  Checking git dirtiness of "${repo_dir}"`)
+			return get_command_output(`git`, {
+				params: 'diff-index --quiet HEAD --'.split(' '),
+				cwd: repo_dir
+			})
+			.catch(() => is_repo_dirty = true)
+		})
+
+
+	const actions = observations
+		.then(() => {
+			console.log(`  git fetch for "${repo_dir}"`)
+			return get_command_output(`git`, {
+					params: 'fetch'.split(' '),
+					//stdio: ['pipe', process.stdout, 'pipe' ],
+					cwd: repo_dir,
+					merge_stderr: true
+				})
+				.then(output => console.log(stylize_string.dim(`» git fetch for "${repo_dir}" =>\n${output}`)))
+		})
+		.then(() => {
+			console.log(`  git pull for "${repo_dir}"`)
+			return get_command_output(`git`, {
+					params: 'pull'.split(' '),
+					//stdio: ['pipe', process.stdout, 'pipe' ],
+					cwd: repo_dir,
+					merge_stderr: true
+				})
+				.then(output => console.log(stylize_string.dim(`» git pull for "${repo_dir}" =>\n${output}`)))
+				.catch(err => {
+					if (err.message.includes('There is no tracking information')) { /* swallow */ return}
+					throw err
+				})
+		})
+
+	return actions
+}
+
+
+function update_npm_related(mod_dir, options) {
+	console.log('  update_npm_related', tildify(mod_dir), options)
+
+	const package_json_path = path.join(mod_dir, 'package.json')
+	let package_json
+
+	const observations = Promise.resolve(true)
+		.then(() => {
+			console.log(`  Reading package.json ${tildify(package_json_path)}`)
+			return json.read(package_json_path)
+				.then(s => package_json = s)
+		})
+
+	const actions = observations
+		.then(() => {
+			if (!_.isString(package_json.author) || !package_json.author.includes('Offirmo'))
+				return console.log(`  ${log_symbols.info} "${mod_dir}" skipping npm link since author != Offirmo`, package_json.author)
+
+			console.log(`  npm link for "${mod_dir}"`)
+			return get_command_output(`npm`, {
+					params: 'link'.split(' '),
+					//stdio: ['pipe', process.stdout, 'pipe' ],
+					cwd: mod_dir,
+					merge_stderr: true
+				})
+				.then(output => console.log(stylize_string.dim(output)))
+		})
+
+	return actions
+}
